5673268d31c1fdb39c83ebd7c998adfaf0a6326e
Aniguza <guzmanana0106@gmail.com>
2025-09-29 23:59:32 -0500

    cambiourl

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router";
-import { Navbar } from "./Components/Navbar";
+import { Navbar } from "./components/Navbar";
 import './App.css';  
 
 // Páginas
